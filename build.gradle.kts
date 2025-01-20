23b1111f9821c537f0e3c03a1dcb6e89c679016e
rxdcxdrnine <rkckr1@naver.com>
2025-01-21 02:01:49 +0900

    remove micrometer-core dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,10 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("io.micrometer:micrometer-registry-prometheus")
+    // without micrometer-core
+    // implementation("org.springframework.boot:spring-boot-starter-actuator")
+    // implementation("io.micrometer:micrometer-registry-prometheus")
 
     // use jdbc for testcontainers
     implementation("com.mysql:mysql-connector-j")
